be68364f316c25157b628d9cba9a0c8bd2694b9b
MaxD <max.amb@mail.ru>
2017-07-15 14:47:05 +0300

    web config

---
+++ src/main/java/com/pluralsight/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.pluralsight.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Max on 07.07.2017.
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping(value="/greeting")
+    public String sayHello(Model model) {
+
+        model.addAttribute("greeting", "Hello, World!");
+        return "hello";
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Max
+  Date: 13.05.2017
+  Time: 21:15
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <%--<meta http-equiv="Content-Type" content="text/html"; charset=ISO-8859-1">--%>
+    <title>Index</title>
+</head>
+<body>
+    <h1>
+    Hello World
+    </h1>
+</body>
+</html>
