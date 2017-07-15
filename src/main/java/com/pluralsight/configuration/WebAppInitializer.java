ee2e54e3fffe2029f2e48946832733c88aa98762
MaxD <max.amb@mail.ru>
2017-07-15 15:06:33 +0300

    web config

--- src/main/java/com/pluralsight/configuration/WebAppInitializer.java
+++ src/main/java/com/pluralsight/configuration/WebAppInitializer.java
@@ -19,7 +19,7 @@ public class WebAppInitializer implements WebApplicationInitializer {
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcherServlet", new DispatcherServlet(context));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("*.html");
-        dispatcher.addMapping("/*.pdf");
+        dispatcher.addMapping("*.pdf");
 
     }
 
