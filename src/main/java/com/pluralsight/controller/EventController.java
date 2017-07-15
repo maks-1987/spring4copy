6f6793e542f78df1637f7dd5a83a97339bebc0b8
MaxD <max.amb@mail.ru>
2017-07-15 15:34:58 +0300

    web config

--- src/main/java/com/pluralsight/controller/EventController.java
+++ src/main/java/com/pluralsight/controller/EventController.java
@@ -1,5 +1,22 @@
 package com.pluralsight.controller;
 
+import com.pluralsight.model.Event;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class EventController {
+
+    @RequestMapping(value = "/event", method = RequestMethod.GET)
+    public String displayEventPage(Model model) {
+
+        Event event = new Event();
+        event.setName("Java User Group");
+        model.addAttribute("event", event);
+        return "event";
+    }
+
+
 }
