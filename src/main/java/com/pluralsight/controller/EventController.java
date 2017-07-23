0c3a9be4561105b3b9c68ef02c0c19db68f1cd19
MaxD <max.amb@mail.ru>
2017-07-23 16:00:32 +0300

    [2017-07-15 04:25:22,859] Artifact spring3:war exploded: Error during artifact deployment. See server log for details.

--- src/main/java/com/pluralsight/controller/EventController.java
+++ src/main/java/com/pluralsight/controller/EventController.java
@@ -3,6 +3,7 @@ package com.pluralsight.controller;
 import com.pluralsight.model.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,6 +16,10 @@ public class EventController {
         Event event = new Event();
         event.setName("Java User Group");
         model.addAttribute("event", event);
+        return "event";
+    }
+    @RequestMapping(value = "/event", method = RequestMethod.POST)
+    public  String processEvent(@ModelAttribute("event") Event event) {
         return "event";
     }
 }
