10367bbba3e5853d165faec2e770aadac32b9d0c
MaxD <max.amb@mail.ru>
2017-07-29 12:45:52 +0300

    add I18N internationalisation, add javax validation and hibernete validation

--- src/main/java/com/pluralsight/controller/AttendeeController.java
+++ src/main/java/com/pluralsight/controller/AttendeeController.java
@@ -3,9 +3,12 @@ package com.pluralsight.controller;
 import com.pluralsight.model.Attendee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 public class AttendeeController {
@@ -18,8 +21,13 @@ public class AttendeeController {
         return "attendee";
     }
     @RequestMapping(value = "/attendee", method = RequestMethod.POST)
-    public String processAttendee(@ModelAttribute("attendee") Attendee attendee) {
+    public String processAttendee(@Valid Attendee attendee, BindingResult result, Model m) {
         System.out.println(attendee);
+
+        if(result.hasErrors()) {
+            return "attendee";
+        }
+
         return "redirect:index.html";
 
     }
--- src/main/java/com/pluralsight/model/Attendee.java
+++ src/main/java/com/pluralsight/model/Attendee.java
@@ -1,9 +1,18 @@
 package com.pluralsight.model;
 
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.Size;
+
 public class Attendee {
 
+    @Size(min=2, max=30)
     private String name;
+
+    @NotEmpty
+    @Email
     private String emailAddress;
 
     public String getName() {
